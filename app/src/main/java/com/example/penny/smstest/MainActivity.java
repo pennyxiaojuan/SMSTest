9c81cbfe5a86a2409f6b8ee461fc9acf741c3cad
penny_xiaojuan <penny_xiaojuan@outlook.com>
2016-10-25 15:03:34 +0800

    message

--- app/src/main/java/com/example/penny/smstest/MainActivity.java
+++ app/src/main/java/com/example/penny/smstest/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView content;
     private IntentFilter receiveFilter;
     private MessageReceiver messageReceiver;
+    private EditText to;
+    private EditText msgInput;
+    private Button send;
+    private MessageReceiver messageReceiver1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +29,11 @@ public class MainActivity extends AppCompatActivity {
         content = (TextView) findViewById(R.id.content);
         receiveFilter = new IntentFilter();
         receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
-        MessageReceiver = new MessageReceiver();
+        messageReceiver = new MessageReceiver();
         registerReceiver(messageReceiver,receiveFilter);
     }
     @Override
-    protected void onDestory(){
+    protected void onDestroy(){
         super.onDestroy();
         unregisterReceiver(messageReceiver);
     }
@@ -38,17 +45,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent){
             Bundle bundle = intent.getExtras();//从Intent参数中取出了一个Bundle对象
-            Object[] puds = (Object[]) bundle.get("pdus");//提取短信消息，使用pdu密钥
-            SmsManager[] messages = new SmsManager[pdus.length];
+            Object[] pdus = (Object[]) bundle.get("pdus");//提取短信消息，使用pdu密钥
+            SmsMessage[] messages = new SmsMessage[pdus.length];
 
             for (int i= 0; i<messages.length; i++){
-                messages[i] = SmsManager.createFrompdu((byte[]) pdus[i]);
+                messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
             }
-            SmsManager address = messages[0].getOriginatingAddress();//获取发送方号码
+            String address = messages[0].getOriginatingAddress();//获取发送方号码
             String fullMessage = "";
 
-            for (SmsManager message : messages){
-                fullMessage += message.getmessageBody();//获取短信内容
+            for (SmsMessage message : messages){
+                fullMessage += message.getMessageBody();//获取短信内容
             }
             sender.setText(address);
             content.setText(fullMessage);
