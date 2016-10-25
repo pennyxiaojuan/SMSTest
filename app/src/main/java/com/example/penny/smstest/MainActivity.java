6319730149b1cfd666e31f1f49becd5608bb174e
penny_xiaojuan <penny_xiaojuan@outlook.com>
2016-10-25 16:50:45 +0800

    发送短信成功

--- app/src/main/java/com/example/penny/smstest/MainActivity.java
+++ app/src/main/java/com/example/penny/smstest/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.penny.smstest;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private TextView sender;
     private TextView content;
     private IntentFilter receiveFilter;
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText to;
     private EditText msgInput;
     private Button send;
+    private IntentFilter sendFilter;
+    private SendStatusReceiver sendStatusReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +36,35 @@ public class MainActivity extends AppCompatActivity {
         receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         messageReceiver = new MessageReceiver();
         registerReceiver(messageReceiver,receiveFilter);
+
+        to = (EditText) findViewById(R.id.to);
+        msgInput = (EditText) findViewById(R.id.msg_input);
+        send = (Button) findViewById(R.id.send);
+
+        sendFilter = new IntentFilter();
+        sendFilter.addAction("SENT_SMS_ACTION");
+        sendStatusReceiver = new SendStatusReceiver();
+        registerReceiver(sendStatusReceiver,sendFilter);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+
+                Intent sentIntent = new Intent("SENT_SMS_ACTION");
+                PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this,0,sentIntent,0);
+                smsManager.sendTextMessage(to.getText().toString(),null,msgInput.getText().toString(),pi,null);
+                smsManager.sendTextMessage(to.getText().toString(),null,
+                        msgInput.getText().toString(), null,null);
+
+            }
+        });
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
         unregisterReceiver(messageReceiver);
+        unregisterReceiver(sendStatusReceiver);
     }
     /**
      * 创建一个广播接收器来接收系统发出的短信广播。在MainActivity新建MessageReceive内部类继承自
@@ -58,6 +88,19 @@ public class MainActivity extends AppCompatActivity {
             }
             sender.setText(address);
             content.setText(fullMessage);
+        }
+    }
+
+    class SendStatusReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context,Intent intent){
+            if (getResultCode()== RESULT_OK){
+                //发送短信成功
+                Toast.makeText(context, "Send succeed", Toast.LENGTH_SHORT).show();
+            }else{
+                //发送短信失败
+                Toast.makeText(context, "Send failed", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
